424f324984221447c4e25759e5177c04298e236f
htt2k5b@gmail.com <you@example.com>
2026-07-25 20:31:45 +0000

    doi file cau hinh nextJS

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", 
   experimental: {
     serverActions: {
-      bodySizeLimit: "100mb", 
+      bodySizeLimit: "100mb",
     },
   },
 };
